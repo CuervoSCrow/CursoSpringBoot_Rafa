d6c79fa9ec25585d6390eabd22a95e0903f39fef
Cuervo <vidalcuervo@gmail.com>
2026-01-04 04:20:49 -0600

    Video 3 Creacion de l aprimera API REST

---
+++ CursoSpringBoot_video_03/src/main/java/com/laboratorio/CursoSpringBoot_video_03/CursoSpringBootVideo03Application.java
@@ -0,0 +1,13 @@
+package com.laboratorio.CursoSpringBoot_video_03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootVideo03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootVideo03Application.class, args);
+	}
+
+}
---
+++ CursoSpringBoot_video_03/src/test/java/com/laboratorio/CursoSpringBoot_video_03/CursoSpringBootVideo03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laboratorio.CursoSpringBoot_video_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringBootVideo03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
