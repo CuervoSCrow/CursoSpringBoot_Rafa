fc9eed12a30e5c16d5184d35e3b2413fba61e472
Cuervo <vidalcuervo@gmail.com>
2026-01-04 22:36:45 -0600

    Video 5 Polimorfismo e Inyeccion de Dependencias

---
+++ CursoSpringBoot_video_05/src/main/java/com/laboratorio/CursoSpringBoot_video_05/CursoSpringBootVideo05Application.java
@@ -0,0 +1,13 @@
+package com.laboratorio.CursoSpringBoot_video_05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootVideo05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootVideo05Application.class, args);
+	}
+
+}
