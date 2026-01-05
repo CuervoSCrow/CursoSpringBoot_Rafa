5079a8cd29ea2d8e519c411d24803a8fee938343
Cuervo <vidalcuervo@gmail.com>
2026-01-04 19:55:40 -0600

    Video 4 La capa de Servicios

---
+++ CursoSpringBoot_video_04/src/main/java/com/laboratorio/cursospringboot_video_04/controller/ProductoRestController.java
@@ -0,0 +1,39 @@
+package com.laboratorio.cursospringboot_video_04.controller;
+
+import com.laboratorio.cursospringboot_video_04.model.Producto;
+import com.laboratorio.cursospringboot_video_04.service.ProductoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductoRestController {
+
+    private ProductoService productoService = new ProductoService();
+
+    @GetMapping(value = "/productos/{id}")
+    public Producto findById(@PathVariable Integer id){
+        return this.productoService.findById(id);
+    }
+
+    @GetMapping(value = "/productos")
+    public List<Producto> findAll(){
+        return productoService.findAll();
+    }
+
+    @PostMapping(value = "/productos")
+    public Producto create(@RequestBody Producto producto){
+        return this.productoService.create(producto);
+    }
+
+    @PutMapping(value = "/productos/{id}")
+    public Producto update(@PathVariable Integer id, @RequestBody Producto producto){
+        return this.productoService.update(id,producto);
+    }
+
+    @DeleteMapping(value="/productos/{id}")
+    public String delete(@PathVariable Integer id){
+        return this.productoService.delete(id);
+    }
+
+}
