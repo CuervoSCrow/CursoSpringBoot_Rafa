a137020ba75ada2c7087b1496b685c6c7d9efd16
Cuervo <vidalcuervo@gmail.com>
2026-01-10 14:12:47 -0600

    video 11 Dependencias DevTools y Actuator

---
+++ CursoSpringBoot_video_11/src/main/java/com/laboratorio/CursoSpringBoot_video_11/service/ProductoServiceImpl.java
@@ -0,0 +1,67 @@
+package com.laboratorio.CursoSpringBoot_video_11.service;
+
+import com.laboratorio.CursoSpringBoot_video_11.model.Producto;
+import com.laboratorio.CursoSpringBoot_video_11.util.exception.InvalidDataException;
+import org.springframework.stereotype.Service;
+import tools.jackson.core.PrettyPrinter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductoServiceImpl implements ProductoService{
+
+    List<Producto> productos = new ArrayList<>(
+            List.of(
+                    new Producto(1,"Mouse",10),
+                    new Producto(2,"Teclado",12),
+                    new Producto(3,"Monitor",80)
+            )
+    );
+
+    @Override
+    public Optional<Producto> findById(Integer id) {
+        return productos.stream()
+                .filter(p->p.getCodigo().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public List<Producto> findAll() {
+        return productos;
+    }
+
+    @Override
+    public Producto create(Producto producto) {
+        if(producto.getPrecio()>3000){
+            throw new InvalidDataException("Precio Invalido");
+        }
+        productos.add(producto);
+        return producto;
+    }
+
+    @Override
+    public Optional<Producto> update(Integer id, Producto producto) {
+       if(producto.getPrecio()>3000){
+           throw new InvalidDataException("Precio Invalido");
+       }
+       Optional<Producto> productoAModificar = this.findById(id);
+       if(productoAModificar.isEmpty()){
+           return Optional.empty();
+       }
+       productoAModificar.get().setNombre(producto.getNombre());
+       productoAModificar.get().setPrecio(producto.getPrecio());
+       return productoAModificar;
+    }
+
+    @Override
+    public boolean delete(Integer id) {
+        Optional<Producto> productoAEliminar= this.findById(id);
+        if(productoAEliminar.isEmpty()){
+            return false;
+        }
+        productos.remove(productoAEliminar.get());
+        return true;
+    }
+}
